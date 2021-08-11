35c3e10236049de95fe67c9d130fcb6c29a5ae12
smile2682 <smilecrystal.liyi@gmail.com>
2021-08-11 05:47:58 +0000

    day 2 accomplished!

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,12 +4,14 @@ console.log("Oh hi! r u ready??")
 $(document).ready(function () {
   $("#tweet-text").on('input', function () {
     // caculating the left length of the text
-    const value = $ (this).val();
-    const length = value.length;
+    const $value = $ (this).val();
+    const length = $value.length;
     const left = 140 - length;
     // pass the left value to the button
-    const countButton = $ (".counter").text(left);
+    const $countButton = $ (".counter");
+    $countButton.text(left);
     // when length exceeds 140, add a new class for css purpose;
-    const warningButton = $ (".counter").toggleClass("exceeded",left<0);
+    const warningButton = $countButton.toggleClass("exceeded",left<0);
   })
 });
+
