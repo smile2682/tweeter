58998844e5c2c32bfe71ef1fba30866cc5676323
smile2682 <smilecrystal.liyi@gmail.com>
2021-08-13 03:02:19 +0000

    final check and code clean up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,15 @@
-console.log("Oh hi! r u ready??")
-
-
+// when the doc is ready and textarea is clicked:
 $(document).ready(function () {
   $("#tweet-text").on('input', function () {
     // caculating the left length of the text
-    const $value = $ (this).val();
+    const $value = $(this).val();
     const length = $value.length;
     const left = 140 - length;
     // pass the left value to the button
-    const $countButton = $ (".counter");
+    const $countButton = $(".counter");
     $countButton.text(left);
     // when length exceeds 140, add a new class for css purpose;
-    const warningButton = $countButton.toggleClass("exceeded",left<0);
-  })
+    const warningButton = $countButton.toggleClass("exceeded", left < 0);
+  });
 });
 
