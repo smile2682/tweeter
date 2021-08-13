a20e0547cd3798d6ff39e3796ca8e64995f84b17
smile2682 <smilecrystal.liyi@gmail.com>
2021-08-13 02:56:25 +0000

    final check on the functions and clean up the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,112 +5,81 @@
  */
 
 $(document).ready(function () {
+  // when the page is loaded properly, do the following:
+
+  //1.create tweet element function
   const createTweetElement = (data) => {
+    // define variables matching data in the tweets database
     const $username = $('<h2 class = "username">').text(data.user.name);
     const $image = $('<img/>').attr('src', data.user.avatars);
     const $tweetname = $('<h2 class="tweet-name">').text(data.user.handle);
     const $content = $('<p class = "content">').text(data.content.text);
     const $date = $("<span class='dateCreated'>").text(timeago.format(data.created_at));
-    const $flag = $('<i id="flag" class="fas fa-flag">');
-    const $retweet = $ ('<i id="retweet" class="fas fa-retweet">');
-    const $heart = $('<i id="heart" class="fas fa-heart">');
-
+    const $flag = $('<i class="fas fa-flag flag">');
+    const $retweet = $('<i class="fas fa-retweet retweet">');
+    const $heart = $('<i class="fas fa-heart heart">');
+    //below variables are "structures" components of the html of history tweets
     const $article = $('<article class="tweet">');
     const $header = $('<header>');
     const $profileDiv = $('<div class="profile">');
     const $hr = $('<hr class="line" />');
     const $spanSymbol = $('<span class="symbol">');
     const $footer = $('<footer>');
-
-    $spanSymbol.append($flag,$retweet, $heart);
-    $footer.append($date,$spanSymbol);
+    // append database variables into structures and append structures into article- the single container for every history tweet.
+    $spanSymbol.append($flag, $retweet, $heart);
+    $footer.append($date, $spanSymbol);
     $header.append($profileDiv, $tweetname);
-    $profileDiv.append($image, $username)
-    $article.append($header, $content, $hr, $footer)
-    // $allTweets.append($article);
-
-
+    $profileDiv.append($image, $username);
+    $article.append($header, $content, $hr, $footer);
+    // this func returns the structured history tweets
     return $article;
-
   }
 
+  // 2.render the structured tweets on the single page app
   const renderTweets = function (tweets) {
-  //  empty posts on the screen before posting the database
-  const $allTweets = $('.all-tweets');
+    //  when the tweet button is hit, empty posts on the screen before posting the database
+    const $allTweets = $('.all-tweets');
     $allTweets.empty();
 
     // loops through tweets
     for (const tweet of tweets) {
       // calls createTweetElement for each tweet
       const $tweet = createTweetElement(tweet);
-      // console.log(tweet)
       // takes return value and appends it to the tweets container
       $('.all-tweets').prepend($tweet);
     }
   };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
-  // renderTweets(data);
-
+  // 3.when the submission button is hit:
   const $form = $('#new-post-form');
   $form.on('submit', function (event) {
+    // this prevents html to defaultly post to another page
     event.preventDefault();
+    // define a variable as the countbutton and a variable to show number of characters input
     const $countButton = $(".counter");
     const tweetNumber = Number($countButton.text())
+    // if there is an exsiting error messege, slide it up after hitting submission button
     $('#too-long').slideUp();
     $('#empty').slideUp();
+    // show the corresponding error msg when an error occurs
     if (tweetNumber === 140) {
-      // $('#empty').slideUp();
       return $('#empty').slideDown();
-
     };
     if (tweetNumber < 0) {
-     
       return $('#too-long').slideDown();
     }
-
-    console.log('the form has submitted');
-    // why this = textarea inputs instead of the form as a whole html section?
+    // when the form is submitted, serialize the input
     const serializedData = $(this).serialize();
-    console.log(serializedData);
+    // post the serialized data to the database, then fire the loadTweets func to load it in the all-tweets section
     $.post('/tweets', serializedData).then(loadTweets)
-    // how to slideUp if the client made a second mistake?
-    // $('#too-long').slideUp();
-    // $('#empty').slideUp();
-
-    // why this does not work?
+    // after hitting submit button, textarea reset to null and countbutton reset to 140
     const $newPostForm = $('#tweet-text');
-    // $newPostForm.empty();
     $newPostForm.val('')
-    // .focus();
     $countButton.text(140)
   });
 
+  // create a func to load the tweets from the database
   const loadTweets = () => {
     $.ajax({
       url: '/tweets',
@@ -125,22 +94,4 @@ $(document).ready(function () {
       }
     });
   };
-
-
-  // loadTweets();
-
-
-
-  // $.post('/api/posts', serializedData).then(fetchPosts)
-  // $.post('/api/posts', serializedData).then(() => {})
-  // $.post('/api/posts', serializedData, (response) => {
-  //   console.log(response);
-  //   fetchPosts();
-  // });
-  // });
-  // const $tweet = createTweetElement(tweetData);
-
-  // // Test / driver code (temporary)
-  // console.log($tweet); // to see what it looks like
-  // $('.all-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 })
